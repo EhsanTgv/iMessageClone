84956f6277aa9d5166e51fabcc0a9518eff47c24
EhsanTgv <EhsanTgv@gmail.com>
2023-05-02 17:31:44 +0300

    Initial Commit

---
+++ iMessageClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iMessageClone
+//
+//  Created by Ehsan Taghavi on 2.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iMessageClone/iMessageCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageCloneApp.swift
+//  iMessageClone
+//
+//  Created by Ehsan Taghavi on 2.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct iMessageCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
