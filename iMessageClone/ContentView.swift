db1287fc80bb4999df7274df70c67e9a7c8e42b2
EhsanTgv <EhsanTgv@gmail.com>
2023-05-12 14:50:51 +0300

    Implement Pinned channels

--- iMessageClone/ContentView.swift
+++ iMessageClone/ContentView.swift
@@ -16,49 +16,48 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ChannelList(
-                factory: viewFactory,
-                channels: viewModel.channels,
-                selectedChannel: $viewModel.selectedChannel,
-                swipedChannelId: $viewModel.swipedChannelId,
-                scrollable: true,
-                onlineIndicatorShown: viewModel.onlineIndicatorShown(for:),
-                imageLoader: channelHeaderLeader.image(for:),
-                onItemTap: { channel in
-                    viewModel.selectedChannel = channel.channelSelectionInfo
-                },
-                onItemAppear: viewModel.checkForChannels(index:),
-                channelNaming: viewModel.name(forChannel:),
-                channelDestination: viewFactory.makeChannelDestination(),
-                trailingSwipeRightButtonTapped: viewModel.onDeleteTapped(channel:),
-                trailingSwipeLeftButtonTapped: viewModel.muteTapped(_:),
-                leadingSwipeButtonTapped: {_ in }
-            )
-            .alert(isPresented: $viewModel.alertShown) {
-                switch viewModel.channelAlertType {
-                case let .deleteChannel(channel):
-                    return Alert(
-                        title: Text("Delete channel"),
-                        message: Text("Are you sure you want to delete this channel?"),
-                        primaryButton: .destructive(Text("Delete")) {
-                            viewModel.delete(channel: channel)
-                        },
-                        secondaryButton: .cancel()
-                    )
-                default:
-                    return Alert.defaultErrorAlert
+            ScrollView {
+                if viewModel.pinnedChannels.count > 0 {
+                    PinnedChannelsView(viewModel: viewModel, channelHeaderLoader: channelHeaderLeader, factory: viewFactory)
+                }
+                ChannelList(
+                    factory: viewFactory,
+                    channels: viewModel.channels,
+                    selectedChannel: $viewModel.selectedChannel,
+                    swipedChannelId: $viewModel.swipedChannelId,
+                    scrollable: false,
+                    onlineIndicatorShown: viewModel.onlineIndicatorShown(for:),
+                    imageLoader: channelHeaderLeader.image(for:),
+                    onItemTap: { channel in
+                        viewModel.selectedChannel = channel.channelSelectionInfo
+                    },
+                    onItemAppear: viewModel.checkForChannels(index:),
+                    channelNaming: viewModel.name(forChannel:),
+                    channelDestination: viewFactory.makeChannelDestination(),
+                    trailingSwipeRightButtonTapped: viewModel.onDeleteTapped(channel:),
+                    trailingSwipeLeftButtonTapped: viewModel.muteTapped(_:),
+                    leadingSwipeButtonTapped: viewModel.pinChannelTapped(_:)
+                )
+                .alert(isPresented: $viewModel.alertShown) {
+                    switch viewModel.channelAlertType {
+                    case let .deleteChannel(channel):
+                        return Alert(
+                            title: Text("Delete channel"),
+                            message: Text("Are you sure you want to delete this channel?"),
+                            primaryButton: .destructive(Text("Delete")) {
+                                viewModel.delete(channel: channel)
+                            },
+                            secondaryButton: .cancel()
+                        )
+                    default:
+                        return Alert.defaultErrorAlert
+                    }
+                }
+                .navigationTitle("Message")
+                .toolbar {
+                    iMessageChannelListHeader()
                 }
             }
-            .navigationTitle("Message")
-            .toolbar {
-                iMessageChannelListHeader()
-            }
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(viewFactory: iMessageViewFactory())
     }
 }
--- iMessageClone/iMessageChannelListViewModel.swift
+++ iMessageClone/iMessageChannelListViewModel.swift
@@ -8,10 +8,29 @@
 import Foundation
 import StreamChat
 import StreamChatSwiftUI
+import OrderedCollections
 
 class iMessageChannelListViewModel: ChatChannelListViewModel {
     
     @Injected(\.chatClient) var chatClient
+    
+    @Published var pinnedChannels: OrderedSet<ChatChannel> = []
+    
+    var nonPinnedChannels: LazyCachedMapCollection<ChatChannel> {
+        self.channels
+            .filter({ channel in
+                !self.pinnedChannels.contains(channel)
+            })
+            .lazyCachedMap{ $0 }
+    }
+    
+    func pinChannelTapped(_ channel: ChatChannel) {
+        if pinnedChannels.contains(channel) {
+            pinnedChannels.remove(channel)
+        } else {
+            pinnedChannels.append(channel)
+        }
+    }
     
     func muteTapped(_ channel: ChatChannel) {
         let controller = chatClient.channelController(for: channel.cid)
