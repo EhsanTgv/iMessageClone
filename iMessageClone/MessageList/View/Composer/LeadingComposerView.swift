f78e3871b8e3b7e34d7ebead6e552157f6d7655d
EhsanTgv <EhsanTgv@gmail.com>
2023-10-23 11:51:35 +0300

    Implement LeadingComposerView

---
+++ iMessageClone/MessageList/View/Composer/LeadingComposerView.swift
@@ -0,0 +1,18 @@
+//
+//  LeadingComposerView.swift
+//  iMessageClone
+//
+//  Created by Ehsan Taghavi on 23.10.2023.
+//
+
+import SwiftUI
+
+struct LeadingComposerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeadingComposerView()
+}
