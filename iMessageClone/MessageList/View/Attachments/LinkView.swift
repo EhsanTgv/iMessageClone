8be81e75edcb79cdc2864da247a611702f64082a
EhsanTgv <EhsanTgv@gmail.com>
2023-11-27 15:11:10 +0300

    Implement Custom Attachments

---
+++ iMessageClone/MessageList/View/Attachments/LinkView.swift
@@ -0,0 +1,18 @@
+//
+//  LinkView.swift
+//  iMessageClone
+//
+//  Created by Ehsan Taghavi on 27.11.2023.
+//
+
+import SwiftUI
+
+struct LinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LinkView()
+}
